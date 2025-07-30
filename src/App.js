4f933656429f8cfa94dbae04669c20eb336a137d
rishit1273 <100685166+rishit1273@users.noreply.github.com>
2025-07-31 00:48:43 +0530

    Push full React app with components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+// import Greeting from './Components/Greeting';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import Alert from './Components/Alert';
+import React,{ useState } from 'react';
+import Home from "./Components/Home";
+import About from "./Components/About";
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const[alert, setAlert] = useState(null);
+const showAlert = (message,type) => {
+  setAlert({ //Json object
+    msg: message,
+    type: type
+  });
+  setTimeout(() => {
+    setAlert(null);
+  }, 1500);
+
+}
+  return(
+<Router>
+   
+  <div className="main">
+{/* <div className="hello">
+     <Greeting name="Rishit"/>
+   </div> */}
+   {/* Exact helps in full checing while routing is a good practise*/}
+ <Navbar showAlert={showAlert} />
+ <Alert alert ={alert}/>
+   <Routes>
+    
+     <Route
+            path="/"
+            element={<TextForm title="Enter The Text to Analyze" showAlert={showAlert} />}
+          />
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/about" element={<About />} />
+  </Routes>
+        
+   
+   
+  
+</div>
+   </Router>
   );
 }
 
